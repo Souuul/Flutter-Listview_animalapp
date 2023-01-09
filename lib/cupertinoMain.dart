725c3daa80e6581a714169900a47afce00bf9ad3
Sol Han <hans53400@naver.com>
2023-01-09 23:23:24 +0900

    make cupertino style

---
+++ lib/cupertinoMain.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+
+class CupertinoMain extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CupertinoMain();
+  }
+}
+
+class _CupertinoMain extends State<CupertinoMain> {
+
+  CupertinoTabBar? tabBar;
+
+  @override
+  void initState() {
+    super.initState();
+    tabBar = CupertinoTabBar(items: <BottomNavigationBarItem>[
+      BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+      BottomNavigationBarItem(icon: Icon(CupertinoIcons.add)),
+    ]);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: CupertinoTabScaffold(
+          tabBar: tabBar!,
+          tabBuilder: (context, value) {
+            if (value ==0) {
+              return Container(
+                child: Center(
+                  child: Text('cupertino tab 1'),
+                ),
+              );
+            } else {
+              return Container(
+                child: Center(
+                  child: Text('cupertino tab 2'),
+                ),
+              );
+            }
+          }
+      ),
+    );
+  }
+}
---
+++ lib/sub/main_2.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:listview_example/cupertinoMain.dart';
+
+
+void main() {
+  runApp(CupertinoMain());
+}
