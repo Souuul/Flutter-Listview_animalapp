a7cc7e2fbab2e9538bef1d46d9bf2f9d135ec4c1
Sol Han <hans53400@naver.com>
2023-01-12 02:15:38 +0900

    make cupertino widget

--- lib/cupertinoMain.dart
+++ lib/cupertinoMain.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:listview_example/animalItem.dart';
+import 'package:listview_example/cupertinoWidget/cupertinowidget.dart';
 import 'package:listview_example/iosSub/cupertinoFirstPage.dart';
+import 'package:listview_example/iosSub/cupertinoSecondPage.dart';
+
+void main() {
+  runApp(CupertinoMain());
+}
 
 class CupertinoMain extends StatefulWidget {
   @override
@@ -10,10 +16,8 @@ class CupertinoMain extends StatefulWidget {
 }
 
 class _CupertinoMain extends State<CupertinoMain> {
-
   CupertinoTabBar? tabBar;
   List<Animal> animalList = List.empty(growable: true);
-  
 
   @override
   void initState() {
@@ -21,17 +25,25 @@ class _CupertinoMain extends State<CupertinoMain> {
     tabBar = CupertinoTabBar(items: <BottomNavigationBarItem>[
       BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
       BottomNavigationBarItem(icon: Icon(CupertinoIcons.add)),
+      BottomNavigationBarItem(icon: Icon(CupertinoIcons.add)),
     ]);
-    animalList.add(Animal(animalName: '벌', kind: '곤충', imagePath: 'repo/images/bee.png'));
-    animalList.add(Animal(animalName: '고양이', kind: '포유류', imagePath: 'repo/images/cat.png'));
-    animalList.add(Animal(animalName: '젖소', kind: '포유류', imagePath: 'repo/images/cow.png'));
-    animalList.add(Animal(animalName: '강아지', kind: '포유류', imagePath: 'repo/images/dog.png'));
-    animalList.add(Animal(animalName: '여우', kind: '포유류', imagePath: 'repo/images/fox.png'));
-    animalList.add(Animal(animalName: '원숭이', kind: '포유류', imagePath: 'repo/images/monkey.png'));
-    animalList.add(Animal(animalName: '돼지', kind: '포유류', imagePath: 'repo/images/pig.png'));
-    animalList.add(Animal(animalName: '늑재', kind: '포유류', imagePath: 'repo/images/wolf.png'));
+    animalList.add(
+        Animal(animalName: '벌', kind: '곤충', imagePath: 'repo/images/bee.png'));
+    animalList.add(Animal(
+        animalName: '고양이', kind: '포유류', imagePath: 'repo/images/cat.png'));
+    animalList.add(Animal(
+        animalName: '젖소', kind: '포유류', imagePath: 'repo/images/cow.png'));
+    animalList.add(Animal(
+        animalName: '강아지', kind: '포유류', imagePath: 'repo/images/dog.png'));
+    animalList.add(Animal(
+        animalName: '여우', kind: '포유류', imagePath: 'repo/images/fox.png'));
+    animalList.add(Animal(
+        animalName: '원숭이', kind: '포유류', imagePath: 'repo/images/monkey.png'));
+    animalList.add(Animal(
+        animalName: '돼지', kind: '포유류', imagePath: 'repo/images/pig.png'));
+    animalList.add(Animal(
+        animalName: '늑재', kind: '포유류', imagePath: 'repo/images/wolf.png'));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -39,19 +51,16 @@ class _CupertinoMain extends State<CupertinoMain> {
       home: CupertinoTabScaffold(
           tabBar: tabBar!,
           tabBuilder: (context, value) {
-            if (value ==0) {
+            if (value == 0) {
               return CupertinoFirstPage(
-                animalList : animalList,
+                animalList: animalList,
               );
+            } else if (value == 1){
+              return CupertinoSecondPage(animalList: animalList);
             } else {
-              return Container(
-                child: Center(
-                  child: Text('cupertino tab 2'),
-                ),
-              );
+              return CupertinoOtherPage();
             }
-          }
-      ),
+          }),
     );
   }
 }
